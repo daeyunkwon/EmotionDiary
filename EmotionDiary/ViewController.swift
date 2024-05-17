b922687b3e89c040d6a88c3918486227ecc10b5e
Daeyun <sweet_ray@naver.com>
2024-05-17 17:09:24 +0900

    Initial Commit

---
+++ EmotionDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionDiary
+//
+//  Created by 권대윤 on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
