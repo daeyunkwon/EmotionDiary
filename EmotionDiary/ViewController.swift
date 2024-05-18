7c92ae8e8ab6d7f00cd91fb9653ee6e33c68c324
Daeyun <sweet_ray@naver.com>
2024-05-18 19:04:57 +0900

    Refactor: 감정 버튼 Outlet 변수명과 감정 레이블 Outlet 변수명 개선

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -11,33 +11,33 @@ final class ViewController: UIViewController {
     
     //MARK: - Properties
     
-    @IBOutlet var button1: UIButton!
-    @IBOutlet var button2: UIButton!
-    @IBOutlet var button3: UIButton!
-    @IBOutlet var button4: UIButton!
-    @IBOutlet var button5: UIButton!
-    @IBOutlet var button6: UIButton!
-    @IBOutlet var button7: UIButton!
-    @IBOutlet var button8: UIButton!
-    @IBOutlet var button9: UIButton!
+    @IBOutlet var happyButton: UIButton!
+    @IBOutlet var loveButton: UIButton!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var embarrassedButton: UIButton!
+    @IBOutlet var upsetButton: UIButton!
+    @IBOutlet var depressedButton: UIButton!
+    @IBOutlet var boredButton: UIButton!
+    @IBOutlet var anxiousButton: UIButton!
+    @IBOutlet var frustratedButton: UIButton!
     
-    @IBOutlet var label1: UILabel!
-    @IBOutlet var label2: UILabel!
-    @IBOutlet var label3: UILabel!
-    @IBOutlet var label4: UILabel!
-    @IBOutlet var label5: UILabel!
-    @IBOutlet var label6: UILabel!
-    @IBOutlet var label7: UILabel!
-    @IBOutlet var label8: UILabel!
-    @IBOutlet var label9: UILabel!
+    @IBOutlet var happyLabel: UILabel!
+    @IBOutlet var loveLabel: UILabel!
+    @IBOutlet var likeLabel: UILabel!
+    @IBOutlet var embarrassedLabel: UILabel!
+    @IBOutlet var upsetLabel: UILabel!
+    @IBOutlet var depressedLabel: UILabel!
+    @IBOutlet var boredLabel: UILabel!
+    @IBOutlet var anxiousLabel: UILabel!
+    @IBOutlet var frustratedLabel: UILabel!
     
-    lazy var labels: [UILabel] = [label1, label2, label3, label4, label5, label6, label7, label8, label9]
+    lazy var labels: [UILabel] = [happyLabel, loveLabel, likeLabel, embarrassedLabel, upsetLabel, depressedLabel, boredLabel, anxiousLabel, frustratedLabel]
     
-    lazy var labelNames: [UILabel: String] = [label1: "label1", label2: "label2", label3: "label3", label4: "label4", label5: "label5", label6: "label6", label7: "label7", label8: "label8", label9: "label9"]
+    lazy var labelNames: [UILabel: String] = [happyLabel: "happyLabel", loveLabel: "loveLabel", likeLabel: "likeLabel", embarrassedLabel: "embarrassedLabel", upsetLabel: "upsetLabel", depressedLabel: "depressedLabel", boredLabel: "boredLabel", anxiousLabel: "anxiousLabel", frustratedLabel: "frustratedLabel"]
     
     var emotionTexts: [String] = ["행복해 ", "사랑해 ", "좋아해 ", "당황해 ", "속상해 ", "우울해 ", "심심해 ", "불안해 ", "좌절해 "]
     
-    var labelCount: [String: Int] = ["label1": 0, "label2": 0, "label3": 0, "label4": 0, "label5": 0, "label6": 0, "label7": 0, "label8": 0, "label9": 0]
+    var labelCount: [String: Int] = ["happyLabel": 0, "loveLabel": 0, "likeLabel": 0, "embarrassedLabel": 0, "upsetLabel": 0, "depressedLabel": 0, "boredLabel": 0, "anxiousLabel": 0, "frustratedLabel": 0]
     
     
     //MARK: - Life Cycle
@@ -59,25 +59,25 @@ final class ViewController: UIViewController {
     }
     
     func configureUI() {
-        configureEmotionButton(button1, imageName: "slime1")
-        configureEmotionButton(button2, imageName: "slime2")
-        configureEmotionButton(button3, imageName: "slime3")
-        configureEmotionButton(button4, imageName: "slime4")
-        configureEmotionButton(button5, imageName: "slime5")
-        configureEmotionButton(button6, imageName: "slime6")
-        configureEmotionButton(button7, imageName: "slime7")
-        configureEmotionButton(button8, imageName: "slime8")
-        configureEmotionButton(button9, imageName: "slime9")
+        configureEmotionButton(happyButton, imageName: "slime1")
+        configureEmotionButton(loveButton, imageName: "slime2")
+        configureEmotionButton(likeButton, imageName: "slime3")
+        configureEmotionButton(embarrassedButton, imageName: "slime4")
+        configureEmotionButton(upsetButton, imageName: "slime5")
+        configureEmotionButton(depressedButton, imageName: "slime6")
+        configureEmotionButton(boredButton, imageName: "slime7")
+        configureEmotionButton(anxiousButton, imageName: "slime8")
+        configureEmotionButton(frustratedButton, imageName: "slime9")
         
-        configureLabel(label1, index: 0, labelName: "label1")
-        configureLabel(label2, index: 1, labelName: "label2")
-        configureLabel(label3, index: 2, labelName: "label3")
-        configureLabel(label4, index: 3, labelName: "label4")
-        configureLabel(label5, index: 4, labelName: "label5")
-        configureLabel(label6, index: 5, labelName: "label6")
-        configureLabel(label7, index: 6, labelName: "label7")
-        configureLabel(label8, index: 7, labelName: "label8")
-        configureLabel(label9, index: 8, labelName: "label9")
+        configureLabel(happyLabel, index: 0, labelName: "happyLabel")
+        configureLabel(loveLabel, index: 1, labelName: "loveLabel")
+        configureLabel(likeLabel, index: 2, labelName: "likeLabel")
+        configureLabel(embarrassedLabel, index: 3, labelName: "embarrassedLabel")
+        configureLabel(upsetLabel, index: 4, labelName: "upsetLabel")
+        configureLabel(depressedLabel, index: 5, labelName: "depressedLabel")
+        configureLabel(boredLabel, index: 6, labelName: "boredLabel")
+        configureLabel(anxiousLabel, index: 7, labelName: "anxiousLabel")
+        configureLabel(frustratedLabel, index: 8, labelName: "frustratedLabel")
     }
     
     func configureEmotionButton(_ button: UIButton, imageName: String) {
